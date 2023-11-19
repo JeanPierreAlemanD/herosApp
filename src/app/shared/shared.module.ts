599990ee0c4fc97a659e8a61127bd8bb7e496d9b
JeanPierreAlemanD <pierre.aleman.13@gmail.com>
2023-11-19 13:23:27 -0500

    cambios para iniciar proyecto

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Error404PageComponent } from './pages/error404-page/error404-page.component';
+
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [
+    Error404PageComponent
+  ],
+})
+export class SharedModule { }
